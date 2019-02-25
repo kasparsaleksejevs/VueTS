e38473b8ad61efe9d8093802abae17b4e7efe0d6
Kaspars Aleksejevs <20342419+kasparsaleksejevs@users.noreply.github.com>
2019-02-25 22:31:09 +0200

    Add project files.

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const bundleOutputDir = './wwwroot/dist';
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = (env) => {
+    const isDevBuild = !(env && env.prod);
+    const devMode = isDevBuild ? 'development' : 'production';
+
+    return [{
+        mode: devMode,
+        stats: { modules: false },
+        context: __dirname,
+        resolve: { extensions: ['.js', '.ts'] },
+        entry: { 'main': './ClientApp/boot.ts' },
+        module: {
+            rules: [
+                { test: /\.vue\.html$/, include: /ClientApp/, loader: 'vue-loader', options: { loaders: { js: 'ts-loader' } } },
+                {
+                    test: /\.ts$/, include: /ClientApp/, use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                appendTsSuffixTo: [/\.vue\.html$/]
+                            }
+                        }
+                    ]
+                },
+                { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : MiniCssExtractPlugin.loader },
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            ]
+        },
+        output: {
+            path: path.join(__dirname, bundleOutputDir),
+            filename: '[name].js',
+            publicPath: 'dist/'
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'css/main.css',
+            }),
+            new VueLoaderPlugin(),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify(isDevBuild ? 'development' : 'production')
+                }
+            }),
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./wwwroot/dist/vendor-manifest.json')
+            })
+        ].concat(isDevBuild ? [
+            // Plugins that apply in development builds only
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+                // Plugins that apply in production builds only
+                new UglifyJsPlugin(),
+                new ExtractTextPlugin('site.css')
+            ])
+    }];
+};
